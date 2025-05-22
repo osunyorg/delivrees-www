5645e379674c6693ff1d05819205c65fde6244c6
Olivia206 <olivia.simonet206@gmail.com>
2025-05-23 01:23:50 +0200

    style

---
+++ assets/js/pageColor.js
@@ -0,0 +1,19 @@
+class PageColor {
+    constructor () {
+        this.colors = ["#5AFFB3", "#57BCBC", "#FF46B1"];
+        
+        this.listen();
+    }
+
+    listen () {
+        this.randomColor = Math.floor(Math.random() * this.colors.length);
+        this.colorVariable = this.colors[this.randomColor];
+
+        this.setColorVariable();
+    }
+    setColorVariable () {
+        document.documentElement.style.setProperty("--color-accent", this.colorVariable);
+    }
+}
+
+export default new PageColor();
